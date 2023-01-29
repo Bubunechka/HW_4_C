36a11f44f3d240b81b664c91e2e0b158cb0401c7
Алёна Катанская <alyona.katanskaya@gmail.com>
2023-01-29 14:58:15 +0300

    add Ex_29

---
+++ Ex_29/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов
+// и выводит их на экран. Данные вводятся с консоли пользователем
+
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// int Method(string[] args);
+// {
+//     int[] massive = new int[8];
+//     int i = Console.ReadLine();
+    
+//     //int i = 0;
+//     //string result = string.Empty;
+    
+//     for(int i = 0; i<= 8; i++) 
+//     {
+        
+//     }
+
+// }
+// Console.Write("Вывод: ");
+
+int[] a = Enumerable.Range(1, 8).Select(x => 17 % x).ToArray(); 
+foreach (int i in a)
+  Console.WriteLine(i);
